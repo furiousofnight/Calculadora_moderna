aed5fb69c9d96820aa61354dfe6659de19fa92da
furiousofnight <henriquecosta1322d@gmail.com>
2025-03-26 00:36:24 -0300

    up

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import os
 from flask import Flask, render_template
+from datetime import datetime  # Importe isso para usar timestamps únicos
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    now = datetime.now().strftime("%Y%m%d%H%M%S")  # atualiza o timestamp a cada reload da página
+    return render_template('index.html', now=now)
 
 
 if __name__ == '__main__':
